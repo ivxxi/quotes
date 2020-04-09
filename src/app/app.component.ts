afe9b6b2ef22cff44c5ae708959b099c3b10dfed
crystal alice <crystalalice21@gmail.com>
2020-04-09 22:08:50 +0300

    adds app.component ts file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Quote } from './quote';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'Quotes';
+//  quotes:Quote []= [
+//    {id:1, name:'tell me something my friend. You ever danced with the devil in the pale of the moonlight?'},
+  //  {id:2,name:'welcome to the panic room, where all your darkest fears are going to come for you.'},
+    //{id:3,name:'want to know my opening statement? Even though I've been waiting, I'm impatient'},
+    //{id:4,name:'this is the beginning. You're only looking at the Beta.'},
+  //  {id:5,name:'you will finally understand why storms are named after people'},
+    //{id:6,name:'I will never be a morning person, for the moon and I are too much in love. plan'},
+  //];
 }
